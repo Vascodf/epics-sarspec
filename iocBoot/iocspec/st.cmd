6d50f04b3ac83c04cbad783d2f389b46f08dcd02
Vasco Freitas <mailvascofreitas@gmail.com>
2024-04-04 23:22:57 +0100

    Initial import

--- iocBoot/iocspec/st.cmd
+++ iocBoot/iocspec/st.cmd
@@ -11,8 +11,16 @@ cd "${TOP}"
 dbLoadDatabase "dbd/spec.dbd"
 spec_registerRecordDeviceDriver pdbbase
 
+testAsynPortDriverConfigure("testAPD", 1000)
+
 ## Load record instances
 #dbLoadRecords("db/xxx.db","user=vascodf")
+
+dbLoadRecords("db/testAsynPortDriver.db","P=ISTTOK:,R=scope1:,PORT=testAPD,ADDR=0,TIMEOUT=1,NPOINTS=1000")
+dbLoadRecords("${ASYN}/db/asynRecord.db","P=ISTTOK:,R=asyn1,PORT=testAPD,ADDR=0,OMAX=80,IMAX=80")
+
+#asynSetTraceMask("testAPD",0,0xff)
+asynSetTraceIOMask("testAPD",0,0x2)
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
