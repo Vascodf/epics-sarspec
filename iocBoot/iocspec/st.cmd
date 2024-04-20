3f1b8786b355fcdf175cbe4139b64d3972115c2d
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2024-04-21 00:06:24 +0100

    Added Epics IOC files
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- iocBoot/iocspec/st.cmd
+++ iocBoot/iocspec/st.cmd
@@ -11,11 +11,11 @@ cd "${TOP}"
 dbLoadDatabase "dbd/spec.dbd"
 spec_registerRecordDeviceDriver pdbbase
 
-testAsynPortDriverConfigure("testAPD", 1000)
+specAsynPortDriverConfigure("testAPD", 1000)
 
 ## Load record instances
 #dbLoadRecords("db/xxx.db","user=bernardo")
-dbLoadRecords("db/testAsynPortDriver.db","P=ISTTOK:,R=scope1:,PORT=testAPD,ADDR=0,TIMEOUT=1,NPOINTS=1000")
+dbLoadRecords("db/specAsynPortDriver.db","P=ISTTOK:,R=scope1:,PORT=testAPD,ADDR=0,TIMEOUT=1,NPOINTS=1000")
 dbLoadRecords("${ASYN}/db/asynRecord.db","P=ISTTOK:,R=asyn1,PORT=testAPD,ADDR=0,OMAX=80,IMAX=80")
 
 #asynSetTraceMask("testAPD",0,0xff)
